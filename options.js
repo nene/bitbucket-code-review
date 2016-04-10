6bb093c566392fd459359603f8f980b1aae8531c
Rene Saarsoo <nene@triin.net>
2016-04-10 22:10:04 +0300

    Remove copy-paste comments from options.js
    Also switch to using arrow functions.

--- options.js
+++ options.js
@@ -1,24 +1,18 @@
-// Saves options to chrome.storage.sync.
 function saveOptions() {
     chrome.storage.sync.set({
         ignoredAuthors: document.getElementById('ignoredAuthors').value
-    }, function() {
+    }, () => {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
-        setTimeout(function() {
+        setTimeout(() => {
             status.textContent = '';
         }, 750);
     });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restoreOptions() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        ignoredAuthors: ''
-    }, function(items) {
+    chrome.storage.sync.get({ignoredAuthors: ''}, items => {
         document.getElementById('ignoredAuthors').value = items.ignoredAuthors;
     });
 }
