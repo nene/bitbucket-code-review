1cb69836ab613ae1eb1696fd5ac1e6f92eb6b8de
Rene Saarsoo <nene@triin.net>
2016-04-19 20:39:22 +0300

    Keep cache of Commit objects in commitStore
    Ensuring we always use the same object for the same hash,
    avoiding concurrency bugs.

--- src/commitStore.js
+++ src/commitStore.js
@@ -4,14 +4,44 @@ import Commit from "./Commit";
  * Manages storage of commits.
  */
 export default {
+    cache: {},
+
     /**
      * Loads commit with given hash.
      * @param {String}   hash
      * @param {Function} callback Invoked with loaded Commit object
      */
     load(hash, callback) {
-        chrome.storage.local.get(hash, (data) => {
-            callback(new Commit(hash, data[hash]));
+        // Ensure we always use a same Commit instance for the same hash.
+        //
+        // This eliminates concurrency problems: e.g. where we ask for a commit
+        // in two places, modify it separately and save the result.
+        // Without a cache, this would result in last change overriding
+        // previous changes.
+        if (this.cache[hash] instanceof Commit) {
+            callback(this.cache[hash]);
+            return;
+        }
+        // When loading has been started but isn't finished yet,
+        // we wait for the promise to finish.
+        else if (this.cache[hash] instanceof Promise) {
+            this.cache[hash].then(callback);
+            return;
+        }
+        // Perform the actual loading of data.
+        // Until not finished, store the promise to cache.
+        else {
+            this.cache[hash] = this.loadFromStorage(hash);
+            this.cache[hash].then(callback);
+        }
+    },
+
+    loadFromStorage(hash) {
+        return new Promise((resolve) => {
+            chrome.storage.local.get(hash, (data) => {
+                this.cache[hash] = new Commit(hash, data[hash]);
+                resolve(this.cache[hash]);
+            });
         });
     },
 
